832ba04955c6df534eac97661a169461ce93b9c9
Urszula Molska <urszula_molska@wp.pl>
2022-09-15 22:34:04 +0200

    Task 1 done

--- js/task-01.js
+++ js/task-01.js
@@ -5,14 +5,41 @@ const numberOfCategories = Array.from(
 ).length;
 const message = `Number of categories: ${numberOfCategories}`;
 console.log(message);
-//
+
+//Pokazuje liczbę elementów w kategoriach
 
 const categories = document.querySelectorAll("h2");
+const itemElements = document.querySelectorAll(".item");
 
 const categoryList = () => {
-  let sum = 0;
-  categories.forEach((item) => console.log(`Category: ${item}`));
+  [...itemElements].forEach((item) => {
+    console.log(`Category: ${item.firstElementChild.innerText}`);
+
+    let listOfLi = [...item.querySelectorAll("li")];
+    console.log(`Elements: ${listOfLi.length}`);
+  });
 };
+
+categoryList();
+
+
+/** 
+const categories = [...document.querySelectorAll("h2")];
+const itemElements = [...document.querySelectorAll(".item")];
+const nameOfCategories = [...itemElements.firstElementChild];
+
+const categoryList = () => {
+  categories.forEach((item) => {
+    console.log(`Category: ${item.innerText}`);
+  });
+
+  itemElements.forEach((item) => {
+    let listOfLi = [...item.querySelectorAll("li")];
+    console.log(`Elements: ${listOfLi.length}`);
+  });
+};
+
+categoryList();
 
 //console.log(`Elements: ${}`)
 
@@ -27,4 +54,4 @@ const categoryList = () => {
 //categories.forEach((item) => (sum = sum + 1));
 // console.log(`Number of categories: ${sum}`);
 //};
-//numberOfCategories();
+//numberOfCategories()
