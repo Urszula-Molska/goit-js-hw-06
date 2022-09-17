951fd2362096c3cf2cc44e052681ef5073014611
Urszula Molska <urszula_molska@wp.pl>
2022-09-17 22:36:30 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -2,17 +2,16 @@ const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
 
 const counter = document.querySelector("#value");
-
+let counterValue = 0;
 decrementBtn.addEventListener("click", Minus);
 incrementBtn.addEventListener("click", Plus);
 
-let counterValue = 0;
-const Minus = (event) => {
+function Minus(event) {
   return (counterValue -= 1);
-};
+}
 
-const Plus = (event) => {
+function Plus(event) {
   return (counterValue += 1);
-};
+}
 
 counter.innerHTML = `${counterValue}`;
