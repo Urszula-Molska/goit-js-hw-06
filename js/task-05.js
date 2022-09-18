cb1a4c02ceea8c5482dcc556488d59bb016d6b7b
Urszula Molska <urszula_molska@wp.pl>
2022-09-18 17:45:35 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,6 +4,9 @@ const output = document.querySelector("#name-output");
 textInput.addEventListener("input", (event) => {
   if (event.currentTarget.value) {
     output.textContent = event.currentTarget.value;
+  }
+  if ((event.currentTarget.value = " ")) {
+    output.textContent = "Nic nie wpisano";
   } else {
     output.textContent = "Anonymous";
   }
