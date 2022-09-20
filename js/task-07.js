845d2594431aec900a1d00363648e2e91f30006c
Urszula Molska <urszula_molska@wp.pl>
2022-09-20 21:58:13 +0200

    Task nr 7 done

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,7 @@
 const fontSizeControl = document.getElementById("font-size-control");
+const span = document.getElementById("text");
+fontSizeControl.addEventListener("input", (event) => {
+  console.log(event.currentTarget.value);
+  const evCurTar = event.currentTarget.value.toString();
+  span.style.fontSize = `${evCurTar}px`;
+});
