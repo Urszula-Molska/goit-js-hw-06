18a341f9595c1c8ca7411c55727c220acd374ee0
Urszula Molska <urszula_molska@wp.pl>
2022-09-21 21:34:58 +0200

    task nr 9 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,8 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const input = document.querySelector("input");
+
+function createBoxes(amount) {}
+
+input.addEventListener(input, createBoxes(amount));
