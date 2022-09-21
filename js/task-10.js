762a7eb42a08d856a11e1262ccf5c1efce13b9d5
Urszula Molska <urszula_molska@wp.pl>
2022-09-21 23:53:52 +0200

    10 task to fix

--- js/task-10.js
+++ js/task-10.js
@@ -2,7 +2,23 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 const input = document.querySelector("input");
+const boxes = document.querySelector("#boxes");
+const buttonCreate = document.querySelector("button[data-create]");
+const buttonRemove = document.querySelector("button[data-destroy]");
 
-function createBoxes(amount) {}
+input.addEventListener("input", (event) => {
+  const amount = event.currentTarget.value;
+});
 
-input.addEventListener(input, createBoxes(amount));
+function createBoxes(amount) {
+  for (let i = 0; (i = amount); i += 1) {
+    const newHeight = 30 + 10 * i;
+    const newWidth = 30 + 10 * i;
+    const newDiv = `<div class="box">height="${newHeight}px",width="${newWidth}px"</div>`;
+    const toHtml = boxes.insertAdjacentHTML("afterbegin", newDiv);
+    //= getRandomHexColor();
+  }
+}
+
+buttonCreate.addEventListener("click", createBoxes(amount));
+buttonRemove.removeEventListener("click", createBoxes(amount));
