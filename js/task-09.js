260f020fe507d97ea8315bde12114fab50f41006
Urszula Molska <urszula_molska@wp.pl>
2022-09-25 18:15:04 +0200

    Task nr 9- fix

--- js/task-09.js
+++ js/task-09.js
@@ -1,8 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const button = document.querySelector(".change-color");
-const span = document.querySelector(".color");
-button.addEventListener("click", (event) => {
-  span.style.backgroundColor = getRandomHexColor();
+const button = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+const body = document.querySelector('body');
+button.addEventListener('click', event => {
+  body.style.backgroundColor = getRandomHexColor();
+  span.innerHTML = getRandomHexColor();
 });
